5b97775a8e7a2a5880a34f26af578093813cc759
felix <felixfranko66@gmail.com>
2022-10-17 10:33:57 +0300

    seperate card component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Card from "./components/Card";
 import { data as products } from "./helpers/data";
 
 function App() {
@@ -8,31 +9,7 @@ function App() {
         <h1 className="text-center">Bizim Store</h1>
         <div className="row justify-content-center g-3">
           {products.map((product) => {
-            const { category, id, image, price, title } = product;
-            return (
-              <div key={id} className="col">
-                <div className="card m-auto" style={{ width: "18rem" }}>
-                  <img
-                    src={image}
-                    style={{ height: "18rem" }}
-                    className="card-img-top"
-                    alt="..."
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title text-truncate" title={title}>
-                      {title}
-                    </h5>
-                    <h6 className="card-subtitle mb-2 text-muted">
-                      {category}
-                    </h6>
-                  </div>
-                  <div className="card-body d-flex justify-content-between">
-                    <span className="fw-bold">{price}$</span>
-                    <i className="fa-solid fa-cart-plus"></i>
-                  </div>
-                </div>
-              </div>
-            );
+            return <Card key={product.id} {...product} />;
           })}
         </div>
       </div>
