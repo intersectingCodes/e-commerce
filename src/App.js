1bfa1905d168b500eed056f03a0a3e1774108d38
felix <felixfranko66@gmail.com>
2022-10-17 10:18:27 +0300

    add cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { data as products } from "./helpers/data";
+
 function App() {
+  console.log(products);
   return (
     <div className="App">
-      <h1>Bizim Store</h1>
+      <div className="container">
+        <h1>Bizim Store</h1>
+        <div className="row">
+          {products.map((product) => {
+            const { category, id, image, price, title } = product;
+            return (
+              <div key={id} className="col">
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {category}
+                    </h6>
+                  </div>
+                  <div className="card-body">
+                    <span>{price}</span>
+                    <i className="fa-solid fa-cart-plus"></i>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
