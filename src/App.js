3054f6facfb8ffbdaa326535d3917e9b3c2a20d3
felix <felixfranko66@gmail.com>
2022-10-17 10:25:17 +0300

    update card

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Bizim Store</h1>
-        <div className="row">
+        <h1 className="text-center">Bizim Store</h1>
+        <div className="row justify-content-center g-3">
           {products.map((product) => {
             const { category, id, image, price, title } = product;
             return (
               <div key={id} className="col">
-                <div className="card" style={{ width: "18rem" }}>
-                  <img src={image} className="card-img-top" alt="..." />
+                <div className="card m-auto" style={{ width: "18rem" }}>
+                  <img
+                    src={image}
+                    style={{ height: "18rem" }}
+                    className="card-img-top"
+                    alt="..."
+                  />
                   <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
+                    <h5 className="card-title text-truncate" title={title}>
+                      {title}
+                    </h5>
                     <h6 className="card-subtitle mb-2 text-muted">
                       {category}
                     </h6>
                   </div>
-                  <div className="card-body">
-                    <span>{price}</span>
+                  <div className="card-body d-flex justify-content-between">
+                    <span className="fw-bold">{price}$</span>
                     <i className="fa-solid fa-cart-plus"></i>
                   </div>
                 </div>
